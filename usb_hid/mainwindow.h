9a4274f7c184109f963dc08845311a78c7cd7ce3
paul kirsanov <paul.kirsanov@yandex.ru>
2020-05-06 19:19:09 +0300

    060520

---
+++ usb_hid/mainwindow.h
@@ -0,0 +1,43 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "C:\libusb\include/lusb0_usb.h"
+#include <QTimer>
+#include <QMessageBox>
+
+// Device vendor and product id.
+#define MY_VID 0x0483
+#define MY_PID 0x5750
+// Device endpoint(s)
+#define EP_IN 0x81
+#define EP_OUT 0x01
+// Device of bytes to transfer.
+#define BUF_SIZE 3
+// Device configuration and interface id.
+#define MY_CONFIG 1
+#define MY_INTF 0
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    bool connect_status;
+    QTimer Timer;
+private slots:
+    void ReadyCheck(void);
+    void on_pushButton_clicked();
+    void on_pushButton_2_clicked();
+};
+
+#endif // MAINWINDOW_H
